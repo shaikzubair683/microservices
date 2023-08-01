4bcbc274c193085e8daba975d964f08d4882bdef
Shaik Zubair <shaikzubair683@gmail.com>
2023-08-01 12:16:32 -0400

    updated gateway

---
+++ CloudGateway1/src/main/java/com/ShaikZubair/CloudGateway1/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.ShaikZubair.CloudGateway1.controller;
+
+
+import com.ShaikZubair.CloudGateway1.model.AuthenticateResponse1;
+import com.nimbusds.openid.connect.sdk.AuthenticationResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticationController {
+    public ResponseEntity<AuthenticateResponse1> login(
+            @AuthenticationPrincipal OidcUser oidcUser,
+            Model model,
+            @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client
+    ){
+        AuthenticateResponse1 authenticationResponse = AuthenticateResponse1.builder()
+                .userId(oidcUser.getEmail())
+                .accessToken(client.getAccessToken().getTokenValue())
+                .refreshToken(client.getRefreshToken().getTokenValue())
+                .expiresAt(client.getAccessToken().getExpiresAt().getEpochSecond())
+                .authorityList(oidcUser.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .build();
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
+    }
+}
---
+++ CloudGateway1/src/test/java/com/ShaikZubair/CloudGateway1/CloudGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShaikZubair.CloudGateway1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
